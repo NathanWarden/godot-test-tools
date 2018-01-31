6ed1630bbe4748e77fafde77be7ddea883c092ec
Nathan Warden <nathan@nathanwarden.com>
2018-01-31 15:43:14 -0500

    Added more math tests.

--- Godot_Project/Tests/MathTests/MathfLerpTests.cs
+++ Godot_Project/Tests/MathTests/MathfLerpTests.cs
@@ -1,10 +1,53 @@
-﻿using System;
+﻿using Godot;
+using NUnit.Framework;
+
 namespace GodotTestingProject.Tests.MathTests
 {
+	[TestFixture]
 	public class MathfLerpTests
 	{
-		public MathfLerpTests()
+		const float marginOfError = float.Epsilon;
+
+
+		[Test]
+		public void LerpShouldReturnPointFiveWhenHalfWayBetweenZeroAndOne()
 		{
+			Assert.That(Mathf.Lerp(0, 1, 0.5f), Is.EqualTo(0.5f).Within(marginOfError));
+		}
+
+
+		[Test]
+		public void LerpShouldReturnZeroWhenTheWeightIsZeroBetweenZeroAndOne()
+		{
+			Assert.That(Mathf.Lerp(0, 1, 0), Is.EqualTo(0).Within(marginOfError));
+		}
+
+
+		[Test]
+		public void LerpShouldReturnOneWhenTheWeightIsOneBetweenZeroAndOne()
+		{
+			Assert.That(Mathf.Lerp(0, 1, 1), Is.EqualTo(1).Within(marginOfError));
+		}
+
+
+		[Test]
+		public void LerpShouldClampToOneWhenTheWeightIsTwoBetweenZeroAndOne()
+		{
+			Assert.That(Mathf.Lerp(0, 1, 2), Is.EqualTo(1).Within(marginOfError));
+		}
+
+
+		[Test]
+		public void LerpShouldReturnPointFiveWhenTheWeightIsPointFiveBetweenOneAndZero()
+		{
+			Assert.That(Mathf.Lerp(1, 0, 0.5f), Is.EqualTo(0.5f).Within(marginOfError));
+		}
+
+
+		[Test]
+		public void LerpShouldReturnNegativePointFiveWhenWeightIsPointFiveBetweenZeroAndNegativeOne()
+		{
+			Assert.That(Mathf.Lerp(0, -1, 0.5f), Is.EqualTo(-0.5f).Within(marginOfError));
 		}
 	}
 }
--- Godot_Project/Tests/MathTests/MathfMiscTests.cs
+++ Godot_Project/Tests/MathTests/MathfMiscTests.cs
@@ -1,10 +1,106 @@
-﻿using System;
+﻿using Godot;
+using NUnit.Framework;
+
 namespace GodotTestingProject.Tests.MathTests
 {
+	[TestFixture]
 	public class MathfMiscTests
 	{
-		public MathfMiscTests()
+		const float marginOfError = float.Epsilon;
+
+
+		// Mathf consts
+
+		[Test]
+		public void MathfPIShouldEqualDotNetPI()
 		{
+			Assert.That(Mathf.PI, Is.EqualTo(System.Math.PI).Within(0.0000001));
+		}
+
+		// Mathf.Deg2Rad
+
+		[Test]
+		public void Deg2RadOf180ShouldBePI()
+		{
+			Assert.That(Mathf.Deg2Rad(180), Is.EqualTo(Mathf.PI).Within(marginOfError));
+		}
+
+
+		[Test]
+		public void Deg2RadOf270ShouldBePIAndAHalf()
+		{
+			Assert.That(Mathf.Deg2Rad(270), Is.EqualTo(Mathf.PI + Mathf.PI/2).Within(marginOfError));
+		}
+
+
+		[Test]
+		public void Deg2RadOf540ShouldBeThreePI()
+		{
+			Assert.That(Mathf.Deg2Rad(540), Is.EqualTo(Mathf.PI * 3).Within(marginOfError));
+		}
+
+		// Mathf.Rad2Deg
+
+		[Test]
+		public void Rad2DegOfPIShouldBe180()
+		{
+			Assert.That(Mathf.Rad2Deg(Mathf.PI), Is.EqualTo(180).Within(marginOfError));
+		}
+
+
+		[Test]
+		public void Rad2DegOfPIAndAHalfShouldBe270()
+		{
+			Assert.That(Mathf.Rad2Deg(Mathf.PI + Mathf.PI/2), Is.EqualTo(270).Within(marginOfError));
+		}
+
+
+		[Test]
+		public void Rad2DegOfThreePIShouldBe540()
+		{
+			Assert.That(Mathf.Rad2Deg(Mathf.PI * 3), Is.EqualTo(540).Within(marginOfError));
+		}
+
+		// Mathf.Clamp
+
+		[Test]
+		public void ClampFiveBetweenZeroAndTenShouldBeFive()
+		{
+			Assert.That(Mathf.Clamp(5, 0, 10), Is.EqualTo(5));
+		}
+
+
+		[Test]
+		public void ClampFiveBetweenZeroAndFourShouldBeFour()
+		{
+			Assert.That(Mathf.Clamp(5, 0, 4), Is.EqualTo(4));
+		}
+
+
+		[Test]
+		public void ClampFiveBetweenSevenAndTenShouldBeSeven()
+		{
+			Assert.That(Mathf.Clamp(5, 7, 10), Is.EqualTo(7));
+		}
+
+		// Mathf.Decimals
+
+		[Test]
+		public void DecimalCountForOnePointZeroShouldBeZero()
+		{
+			Assert.That(Mathf.Decimals(1.0f), Is.EqualTo(0));
+		}
+
+		[Test]
+		public void DecimalCountForOnePointZeroOneShouldBeTwo()
+		{
+			Assert.That(Mathf.Decimals(1.01f), Is.EqualTo(2));
+		}
+
+		[Test]
+		public void DecimalCountForTenPointZeroShouldBeZero()
+		{
+			Assert.That(Mathf.Decimals(10.0f), Is.EqualTo(0));
 		}
 	}
 }
